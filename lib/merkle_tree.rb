7d9f3c157a799207e4aedc989f58abe5d22887fe
Manoj Dayaram <noj@squareup.com>
2018-02-17 17:39:04 -0800

    simple API

--- lib/merkle_tree.rb
+++ lib/merkle_tree.rb
@@ -10,4 +10,12 @@ module MerkleTree
   def self.create!(*args)
     MerkleTree::Tree.new(*args)
   end
+
+  def self.proof_of_inclusion(tree, value)
+    MerkleTree::Proof.new(merkle_tree: tree, value: value)
+  end
+
+  def self.verify_inclusion(proof, root_hash)
+    proof.verify_inclusion(root_hash)
+  end
 end
